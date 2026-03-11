28fa8b7702f04d8956f6dd18b76d9b031e533dc1
Bhavana Tallapaka <bhavitallapaka2003@gmail.com>
2026-03-11 16:50:43 +0530

    added typing animation

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -11,3 +11,17 @@ btn.textContent="☀️";
 }
 
 });
+
+const text = "I build practical software and data solutions — from backend systems to analytics dashboards and automation workflows.";
+
+let index = 0;
+
+function typeEffect(){
+    if(index < text.length){
+        document.getElementById("typing-text").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 25);
+    }
+}
+
+window.onload = typeEffect;
