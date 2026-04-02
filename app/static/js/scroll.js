9fd2bcab36138f96127e7ccccfa3f6739b7cd12c
Bhavana Tallapaka <bhavitallapaka2003@gmail.com>
2026-04-02 11:57:03 +0530

    updated profile

--- app/static/js/scroll.js
+++ app/static/js/scroll.js
@@ -10,3 +10,12 @@ document.querySelectorAll("section").forEach(sec=>{
 sec.classList.add("reveal");
 observer.observe(sec);
 });
+
+window.addEventListener("scroll", () => {
+const nav = document.querySelector("nav");
+if(window.scrollY > 50){
+nav.classList.add("scrolled");
+}else{
+nav.classList.remove("scrolled");
+}
+});
